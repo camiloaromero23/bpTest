e0d32a36dc1c07a575316b04335c89766387cde9
Camilo Romero <camiloaromero23@gmail.com>
2023-11-23 18:24:23 -0500

    feat: Update modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ModalComponent {
   @Input() showModal: boolean = false;
+  @Input({ required: true }) message!: string;
   @Output() userConfirm = new EventEmitter<boolean>();
 
   cancel() {
